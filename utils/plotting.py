321839b1dcad94ac3f6eb6d7b311cbecec7e4c6d
RolandSherwin <RolandSherwin@protonmail.com>
2021-05-19 20:38:30 +0530

    addaed a helper class for plotting datasets

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,5 +1,9 @@
+from logging import exception
 import numpy as np
+from numpy.lib.arraysetops import isin
+import tensorflow as tf
 import matplotlib.pyplot as plt
+from tensorflow._api.v2 import data
 
 
 class History():
@@ -18,6 +22,7 @@ class History():
             only if 'val' is True
 
         """
+
         plt.plot(self.history["loss"])
         plt.title('Model Loss')
         plt.xlabel('epoch')
@@ -92,16 +97,69 @@ class History():
 
 
 class PreprocessData():
-    """Returns a standard numpy output given tf.dataset input/ anyother input
+    """Returns a standard numpy output given tf.data.Dataset or list of numpy array
+    The first dimension should be the number of examples, eg: (m,_,_,_). If tf.data.Dataset is
+    a batch, then it should be (batches, batch_size, _, _)
+
+    Arguments:
+    dataset - should be tf.data.Dataset or [x,y] where x,y are numpy array
     """
 
-    def __init__(self, dataset=None) -> None:
-        pass
+    def __init__(self, dataset) -> None:
+
+        if isinstance(dataset, list) and isinstance(dataset[0], np.ndarray) and isinstance(dataset[1], np.ndarray):
+            self.kind = 'numpy'
+        elif isinstance(dataset, tf.data.Dataset):
+            # check if it has batches
+            try:
+                dataset._batch_size
+                self.kind = 'tfds_batch'
+            except AttributeError:
+                self.kind = 'tfds'
+        else:
+            raise TypeError(
+                "Only pass in list of numpy array or tf.data.Dataset")
+
+        self.dataset = dataset
+        # print("Kind : " , self.kind)
+
+    def generator(self):
+        """A generator that returns x,y one by one; S
+        """
+        if self.kind == 'numpy':
+            length = self.dataset[0].shape[0]
+
+            for i in range(length):
+                yield self.dataset[0][i], self.dataset[1][i]
+
+        if self.kind == 'tfds':
+            length = int(self.dataset.__len__())
+
+            for x, y in self.dataset.take(length):
+                yield x, y
+
+        if self.kind == 'tfds_batch':
+            num_batches = int(self.dataset.__len__())
+            batch_size = int(self.dataset._batch_size)
+
+            for x_batch, y_batch in self.dataset.take(num_batches):
+                for i in range(batch_size):
+                    yield x_batch[i], y_batch[i]
 
 
 if __name__ == '__main__':
-    history_dict = np.load('../00-LeNet/history.npy',
-                           allow_pickle='TRUE').item()
-    hist = History(history_dict)
-    hist.loss(show_optimal=True)
+    # history_dict = np.load('../00-LeNet/history.npy',
+    #                      allow_pickle='TRUE').item()
+    # hist = History(history_dict)
+    # hist.loss(show_optimal=True)
     # hist.metric(show_optimal=True)
+
+    # ---
+    original_x = np.ones(shape=(10, 10))
+    original_y = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+    d = tf.data.Dataset.from_tensor_slices((original_x, original_y)).batch(2)
+
+    p = PreprocessData(d)
+
+    for test_x, test_y in p.generator():
+        print(test_x, test_y)
